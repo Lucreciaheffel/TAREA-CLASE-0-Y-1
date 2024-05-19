d01d4f18a35e47de3bf1243ffd5c8eef431635a6
Lucreciaheffel <heffellucrecia@gmail.com>
2024-05-19 15:17:29 -0300

    tarea clase 10 javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let nombre = "lucrecia";
+let apellido = "heffel";
+console.log(nombre + " " + apellido);
+let numberone=120;
+let numbertwo=20;
+let resultsuma= numberone + numbertwo;
+console.log("Suma:",resultsuma);
+let resultsumafinal= resultsuma + 15;
+console.log("SUMA FINAL:",resultsumafinal);
